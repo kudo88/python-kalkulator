ad8ddee6299f0706de5df7a98bc09d66dce150c7
Kudryavka Anatolyvevna Strugatskaya <75708159+kudo88@users.noreply.github.com>
2020-12-09 08:44:20 +0700

    awal

---
+++ kalkulator-sekuensial.py
@@ -0,0 +1,67 @@
+import os
+
+cmd = ('+','-','*','/','=','M+','M-','MR','MC',None,'','RESET')
+memori = 0
+hasil = 0.0
+isi,tampil = '',''
+opr = None
+selesai = False
+
+def reset():
+    memori = 0
+    hasil = 0.0
+    isi,tampil = '',''
+    opr = None
+    selesai = False
+
+def hitung(par_isi):
+    global opr, tampil, hasil, memori
+    if opr == '+':
+        hasil += par_isi
+        tampil += ' + '+str(par_isi)
+    elif opr == '-':
+        hasil -= par_isi
+        tampil += ' - '+str(par_isi)
+    elif opr == '*':
+        hasil *= par_isi
+        tampil += ' * '+str(par_isi)
+    elif opr == '/':
+        hasil /= par_isi
+        tampil += ' / '+str(par_isi) 
+    elif opr == 'M+':
+        memori += par_isi
+    elif opr == 'M-':
+        memori -= par_isi
+
+def parse(par_isi):
+    global opr, tampil, hasil
+    if par_isi.isdigit():
+        if opr == None:
+            hasil = float(par_isi)
+            tampil += f' {par_isi}'
+        else:
+            hitung(float(par_isi))
+    else:
+        opr = par_isi
+            
+
+while not selesai:
+    print('Kalkulator sederhana')
+    isi = isi.upper()
+    if isi == 'MR':
+        print(f'Isi memori > {memori}')
+    elif isi == 'MC':
+        memori = 0
+        print(f'Isi memori sudah dibersihkan')
+    if (not isi.isdigit()) and (not isi in cmd):
+        print('Masukkan angka / perintah dengan benar.')
+    if isi == 'RESET':
+        reset
+        print('Berhasil reset')
+    isi = input('input angka / perintah > ')
+    parse(isi)
+    if isi == '=':
+        print(f'Hasil Kalkulasi > {tampil} = {hasil}')
+        selesai = True
+    else:
+        os.system('cls')
